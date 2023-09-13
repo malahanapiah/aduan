9765f654f88b7ca7b7eb2292a06e2ca37d776741
malahanapiah <normala@perak.gov.my>
2023-09-13 10:33:32 +0800

    Add complaint, action, and action status

---
+++ resources/views/complaint/edit.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md 12"></div>
+    </div>
+</div>
+
+@endsection
+
+
+
+@push('style')
+<style>
+    body {
+        background-color: lightblue;
+    }
+    </style>
+
+@endpush
+
+@push('script')
+<!-- <script>
+    alert('welcome to edit page');
+</script> -->
+@endpush
